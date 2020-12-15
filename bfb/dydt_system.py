db22932e1b5cb2e521f149fb0175be1bffbcfb99
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2020-12-15 10:25:50 -0500

    Add more comments to dydt system

--- bfb/dydt_system.py
+++ bfb/dydt_system.py
@@ -5,13 +5,36 @@ import numpy as np
 def dydt(t, y, dz, z, n, ni, params):
     """
     System of ODEs where
-    y[0] = rho_bb
-    y[1] = rho_cc
+    y[0] = rho_bb (ρ︦𝚋)
+    y[1] = rho_cc (ρ︦𝚌)
     y[2] = v
+
+    Parameters
+    ----------
+    t : float
+        Time [s] in func(t, y)
+    y : array
+        Variables in func(t, y)
+    dz : array
+        Step in z-direction, Δz [m]
+    n : float
+        Grid point at top of reactor, total grid points
+    ni : float
+        Grid point at top of bed
+    params : dict
+        Parameters from JSON file
+
+    Returns
+    -------
+    dydt : array
+        System of ODEs
     """
 
     # Variables
     # ------------------------------------------------------------------------
+    # rho_bb    biomass mass concentration, ρ︦𝚋 [kg/m³]
+    # rho_cc    char mass concentration, ρ︦𝚌 [kg/m³]
+    # v         solid fuel velocity, v [m/s]
 
     rho_bb = y[0:n]
     rho_cc = y[n:2 * n]
@@ -19,16 +42,15 @@ def dydt(t, y, dz, z, n, ni, params):
 
     # Constants and parameters
     # ------------------------------------------------------------------------
+    # g    acceleration from gravity [m/s²]
+    # r    universal gas constant [kJ/(mol K)]
+    # ts   solid fuel temperature [K]
 
-    g = 9.81        # acceleration from gravity [m/s²]
-    r = 0.008314    # universal gas constant [kJ/(mol K)]
+    g = 9.81
+    r = 0.008314
 
     # TODO: define temperature in parameters file
-    ts = 773.15     # solid fuel temperature [K]
-
-    # TODO: calculate rho_g, this value is for nitrogen gas
-    # nitrogen gas density [kg/m³]
-    rho_g = 0.422
+    ts = 773.15
 
     d = params['d']
     d_b = params['d_b']
@@ -50,8 +72,8 @@ def dydt(t, y, dz, z, n, ni, params):
     wc = params['wc']
     xc = params['xc']
 
-    #                          Biomass pyrolysis kinetics
-    # ========================================================================
+    # Biomass pyrolysis kinetics
+    # ------------------------------------------------------------------------
 
     a1 = 1.3e8      # biomass -> volatiles frequency factor [1/s]
     a2 = 2.0e8      # biomass -> tar frequency factor [1/s]
@@ -67,42 +89,44 @@ def dydt(t, y, dz, z, n, ni, params):
     sb = -(k1 + k2 + k3) * rho_bb
     sc = k3 * rho_bb
 
-    # ============================= Mass balance =============================
-
-    # Biomass mass concentration ︦ρ𝚋 [kg/m³]
+    # Biomass mass concentration in bed ∂ρ︦𝚋/∂t
     # ------------------------------------------------------------------------
+    # ac         inner cross-section area of reactor bed [m²]
+    # drhobb_dt  biomass mass concentration ∂ρ︦𝚋/∂t where ρ︦𝚋 is [kg/m³]
 
-    ac = (np.pi / 4) * (d**2)  # inner cross-section area of the reactor bed [m²]
+    ac = (np.pi / 4) * (d**2)
 
     # below fuel inlet
-    drhobbdt = np.zeros(n)
-    drhobbdt[0:n1] = - (-rho_bb[1:n1 + 1] * v[1:n1 + 1] + rho_bb[0:n1] * v[0:n1]) / dz[0:n1] + sb[0:n1]
+    drhobb_dt = np.zeros(n)
+    drhobb_dt[0:n1] = - (-rho_bb[1:n1 + 1] * v[1:n1 + 1] + rho_bb[0:n1] * v[0:n1]) / dz[0:n1] + sb[0:n1]
 
     # at fuel inlet
-    drhobbdt[n1] = - (rho_bb[n1] * v[n1] - rho_bb[n1 - 1] * v[n1 - 1]) / dz[n1] + mf / (ac * dz[n1]) + sb[n1]
+    drhobb_dt[n1] = - (rho_bb[n1] * v[n1] - rho_bb[n1 - 1] * v[n1 - 1]) / dz[n1] + mf / (ac * dz[n1]) + sb[n1]
 
     # above fuel inlet in bed
-    drhobbdt[n1 + 1:ni] = - (rho_bb[n1 + 1:ni] * v[n1 + 1:ni] - rho_bb[n1:ni - 1] * v[n1:ni - 1]) / dz[n1 + 1:ni] + sb[n1 + 1:ni]
+    drhobb_dt[n1 + 1:ni] = - (rho_bb[n1 + 1:ni] * v[n1 + 1:ni] - rho_bb[n1:ni - 1] * v[n1:ni - 1]) / dz[n1 + 1:ni] + sb[n1 + 1:ni]
 
-    # Char mass concentration in bed ︦ρ𝚌 [kg/m³]
+    # Char mass concentration in bed ∂ρ︦𝚌/∂t
     # ------------------------------------------------------------------------
+    # drhocc_dt  char mass concentration ∂ρ︦𝚌/∂t where ρ︦𝚌 is [kg/m³]
 
-    drhoccdt = np.zeros(n)
-    drhoccdt[0:ni] = - (-rho_cc[1:ni + 1] * v[1:ni + 1] + rho_cc[0:ni] * v[0:ni]) / dz[0:ni] + sc[0:ni]
-
-    # ========================= Solid phase momentum =========================
+    drhocc_dt = np.zeros(n)
+    drhocc_dt[0:ni] = - (-rho_cc[1:ni + 1] * v[1:ni + 1] + rho_cc[0:ni] * v[0:ni]) / dz[0:ni] + sc[0:ni]
 
     # Momentum exchange with inert bed particles Fʙ'
     # ------------------------------------------------------------------------
+    # d_bub    bubble diameter, dʙ [m]
+    # rho_b    biomass density, ρ𝖻 [kg/m³]
+    # rho_c    char density, ρ𝖼 [kg/m³]
+    # rho_g    gas density, ρɡ [kg/m³]
     # us       superficial gas velocity at inlet, u𝗌𝚏 [m/s]
     # umf      minimum fluidization velocity, u𝗆𝚏 [m/s]
-    # d_bub    bubble diameter, dʙ [m]
-    # u_bub    bubble velocity along bed height, uʙ [m/s]
+    # u_bub    bubble velocity, uʙ [m/s]
     # v_bub    bubble volumetric flux, Vʙ [m/s]
-    # rho_ss   solid fuel mass concentration, ︦ρ𝗌 [kg/m³]
-    # ysc      mass fraction of char in solid fuel [-]
-    # rho_s    solid fuel density, ρ𝗌 [kg/m³]
     # f_bub    force on fuel particles by inert bed material due to bubble flow, Fʙ' [N/m³]
+
+    # TODO: calculate rho_g, this value is for nitrogen gas
+    rho_g = 0.422
 
     # TODO: define pressure and temp in parameters file
     q_lpm = cm.slm_to_lpm(ug, 101.3, 773.15)
@@ -116,22 +140,25 @@ def dydt(t, y, dz, z, n, ni, params):
     u_bub = 12.51 * ((us - umf)**0.362) * ((d_bub / d)**0.52) * d
     v_bub = 1.285 * ((d_bub / d)**1.52) * d
 
-    rho_ss = rho_cc + rho_bb
-    ysc = rho_cc / rho_ss
-    rho_s = ((ysc / rho_c) * ((1 - ysc) / rho_b))**-1
-
     f_bub = -(1 - emf) * rho_p * theta_w * v_bub[0:ni] * (-u_bub[1:ni + 1] + u_bub[0:ni]) / dz[0:ni]
 
     # Momentum exchange with opposite flow βg,𝗌
     # ------------------------------------------------------------------------
-    # psi       biomass shrinkage factor, Ѱ [-]
-    # d_s       average diameter of solid fuel particle, d𝗌 [m]
-    # dr
     # beta_gs   momentum transfer coefficient due to gas drag, βg,𝗌 [N⋅s/m⁴]
+    # cd        here
+    # d_s       average diameter of solid fuel particle, d𝗌 [m]
+    # dr        here
+    # psi       biomass shrinkage factor, Ѱ [-]
+    # re_s      here
+    # rho_ss    solid fuel mass concentration, ︦ρ𝗌 [kg/m³]
+    # ysc       mass fraction of char in solid fuel [-]
 
     # TODO: calculate gas velocity and gas viscosity
     u = us
     mu_g = 3.6e-5
+
+    rho_ss = rho_cc + rho_bb
+    ysc = rho_cc / rho_ss
 
     psi = rho_c / (rho_b * (wc + wa))
     d_s = d_b / (1 + (1.25 * (nf1 * psi * (1 - xc))**(1 / 3) - 1) * ysc)
@@ -147,18 +174,20 @@ def dydt(t, y, dz, z, n, ni, params):
 
     # Momentum exchange with inert particles β𝗉,𝗌
     # ------------------------------------------------------------------------
+    # alpha_p   solids volume fraction of inert bed material, ⍺𝗉 [-]
+    # alpha_s   solids volume fraction of fuel particles, ⍺𝗌 [-]
+    # beta_ps   momentum transfer coefficient due to collision with inert bed particles, β𝗉,𝗌 [N⋅s/m⁴]
     # d_bub_avg bubble diameter averaged over the bed height, ḏʙ [m]
     # de        bed expansion, Δe [-]
     # ef        bed voidage at fluidized state, e𝚏 [-]
-    # alpha_s   solids volume fraction of fuel particles, ⍺𝗌 [-]
-    # alpha_p   solids volume fraction of inert bed material, ⍺𝗉 [-]
     # go        radial distribution function [-]
-    # beta_ps   momentum transfer coefficient due to collision with inert bed particles, β𝗉,𝗌 [N⋅s/m⁴]
+    # rho_s    solid fuel density, ρ𝗌 [kg/m³]
 
     d_bub_avg = np.mean(d_bub[0:ni])
     de = (1 - 0.103 * ((us - umf)**-0.362) * (d_bub_avg / d))**(-1) - 1
     ef = 1 - (1 - emf) / (1 + de)
 
+    rho_s = ((ysc / rho_c) * ((1 - ysc) / rho_b))**-1
     alpha_s = rho_ss / rho_s
     alpha_p = 1 - ef - alpha_s
 
@@ -168,12 +197,13 @@ def dydt(t, y, dz, z, n, ni, params):
     beta_ps2 = (rho_p * d_p**3) + (rho_s * d_s**3)
     beta_ps = (beta_ps1 / beta_ps2) * alpha_p * rho_p * rho_ss * go * np.abs(v)
 
-    # Solid fuel velocity, v [m/s]
+    # Solid fuel velocity ∂v/∂t
     # ------------------------------------------------------------------------
+    # dv_dt     solid fuel velocity ∂v/∂t where v is [m/s]
 
-    dvdt = np.zeros(n)
+    dv_dt = np.zeros(n)
 
-    dvdt[0:ni] = (
+    dv_dt[0:ni] = (
         - v[0:ni] * (v[0:ni] - v[1:ni + 1]) / dz[0:ni]
         + g * (rho_s[0:ni] - rho_g) / rho_s[0:ni]
         + f_bub / rho_s[0:ni]
@@ -182,5 +212,5 @@ def dydt(t, y, dz, z, n, ni, params):
         + v[0:ni] * (sb[0:ni] + sc[0:ni]) / rho_s[0:ni]
     )
 
-    dydt = np.concatenate((drhobbdt, drhoccdt, dvdt))
+    dydt = np.concatenate((drhobb_dt, drhocc_dt, dv_dt))
     return dydt
