103b9ee3763ce5a4b5f62fa3523089e6dde142b1
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2020-12-14 21:32:19 -0500

    Remove old funcs and parameters

--- bfb/dydt_system.py
+++ bfb/dydt_system.py
@@ -1,19 +1,20 @@
+import chemics as cm
 import numpy as np
 
 
-def dydt(t, y, ac, dz, n, ni, ub, vb, params):
+def dydt(t, y, dz, z, n, ni, params):
     """
     System of ODEs where
-    y[0] = rhobb
-    y[1] = rhocc
+    y[0] = rho_bb
+    y[1] = rho_cc
     y[2] = v
     """
 
     # Variables
     # ------------------------------------------------------------------------
 
-    rhobb = y[0:n]
-    rhocc = y[n:2 * n]
+    rho_bb = y[0:n]
+    rho_cc = y[n:2 * n]
     v = y[2 * n:]
 
     # Constants and parameters
@@ -22,19 +23,35 @@ def dydt(t, y, ac, dz, n, ni, ub, vb, params):
     g = 9.81        # acceleration from gravity [m/s²]
     r = 0.008314    # universal gas constant [kJ/(mol K)]
 
-    rhog = 0.422    # nitrogen gas density [kg/m³]
+    # TODO: define temperature in parameters file
     ts = 773.15     # solid fuel temperature [K]
 
-    emf = params['emf']
-    fw = params['fw']
-    mf = params['mf']
-    n1 = params['n1']
-    rhob = params['rhob']
-    rhoc = params['rhoc']
-    rhop = params['rhop']
+    # TODO: calculate rho_g, this value is for nitrogen gas
+    # nitrogen gas density [kg/m³]
+    rho_g = 0.422
 
-    # Biomass pyrolysis kinetics
-    # ------------------------------------------------------------------------
+    d = params['d']
+    d_b = params['d_b']
+    d_p = params['d_p']
+    e = params['e']
+    emf = params['emf']
+    theta_w = params['theta_w']
+    mf = params['mf']
+    nf1 = params["nf1"]
+    phi_p = params['phi_p']
+    psi_s = params['psi_s']
+    mu_c = params['mu_c']
+    n1 = params['n1']
+    rho_b = params['rho_b']
+    rho_c = params['rho_c']
+    rho_p = params['rho_p']
+    ug = params['ug']
+    wa = params['wa']
+    wc = params['wc']
+    xc = params['xc']
+
+    #                          Biomass pyrolysis kinetics
+    # ========================================================================
 
     a1 = 1.3e8      # biomass -> volatiles frequency factor [1/s]
     a2 = 2.0e8      # biomass -> tar frequency factor [1/s]
@@ -47,43 +64,122 @@ def dydt(t, y, ac, dz, n, ni, ub, vb, params):
     k2 = a2 * np.exp(-e2 / (r * ts))
     k3 = a3 * np.exp(-e3 / (r * ts))
 
-    sb = -(k1 + k2 + k3) * rhobb
-    sc = k3 * rhobb
+    sb = -(k1 + k2 + k3) * rho_bb
+    sc = k3 * rho_bb
 
-    # Biomass mass concentration [kg/m³]
+    # ============================= Mass balance =============================
+
+    # Biomass mass concentration ︦ρ𝚋 [kg/m³]
     # ------------------------------------------------------------------------
+
+    ac = (np.pi / 4) * (d**2)  # inner cross-section area of the reactor bed [m²]
 
     # below fuel inlet
     drhobbdt = np.zeros(n)
-    drhobbdt[0:n1] = - (-rhobb[1:n1 + 1] * v[1:n1 + 1] + rhobb[0:n1] * v[0:n1]) / dz[0:n1] + sb[0:n1]
+    drhobbdt[0:n1] = - (-rho_bb[1:n1 + 1] * v[1:n1 + 1] + rho_bb[0:n1] * v[0:n1]) / dz[0:n1] + sb[0:n1]
 
     # at fuel inlet
-    drhobbdt[n1] = - (rhobb[n1] * v[n1] - rhobb[n1 - 1] * v[n1 - 1]) / dz[n1] + mf / (ac * dz[n1]) + sb[n1]
+    drhobbdt[n1] = - (rho_bb[n1] * v[n1] - rho_bb[n1 - 1] * v[n1 - 1]) / dz[n1] + mf / (ac * dz[n1]) + sb[n1]
 
-    # above fuel inlet
-    drhobbdt[n1 + 1:ni] = - (rhobb[n1 + 1:ni] * v[n1 + 1:ni] - rhobb[n1:ni - 1] * v[n1:ni - 1]) / dz[n1 + 1:ni] + sb[n1 + 1:ni]
+    # above fuel inlet in bed
+    drhobbdt[n1 + 1:ni] = - (rho_bb[n1 + 1:ni] * v[n1 + 1:ni] - rho_bb[n1:ni - 1] * v[n1:ni - 1]) / dz[n1 + 1:ni] + sb[n1 + 1:ni]
 
-    # Char mass concentration [kg/m³]
+    # Char mass concentration in bed ︦ρ𝚌 [kg/m³]
     # ------------------------------------------------------------------------
 
-    # below bed top
     drhoccdt = np.zeros(n)
-    drhoccdt[0:ni] = - (-rhocc[1:ni + 1] * v[1:ni + 1] + rhocc[0:ni] * v[0:ni]) / dz[0:ni] + sc[0:ni]
+    drhoccdt[0:ni] = - (-rho_cc[1:ni + 1] * v[1:ni + 1] + rho_cc[0:ni] * v[0:ni]) / dz[0:ni] + sc[0:ni]
 
-    # Solid fuel velocity [m/s]
+    # ========================= Solid phase momentum =========================
+
+    # Momentum exchange with inert bed particles Fʙ'
+    # ------------------------------------------------------------------------
+    # us       superficial gas velocity at inlet, u𝗌𝚏 [m/s]
+    # umf      minimum fluidization velocity, u𝗆𝚏 [m/s]
+    # d_bub    bubble diameter, dʙ [m]
+    # u_bub    bubble velocity along bed height, uʙ [m/s]
+    # v_bub    bubble volumetric flux, Vʙ [m/s]
+    # rho_ss   solid fuel mass concentration, ︦ρ𝗌 [kg/m³]
+    # ysc      mass fraction of char in solid fuel [-]
+    # rho_s    solid fuel density, ρ𝗌 [kg/m³]
+    # f_bub    force on fuel particles by inert bed material due to bubble flow, Fʙ' [N/m³]
+
+    # TODO: define pressure and temp in parameters file
+    q_lpm = cm.slm_to_lpm(ug, 101.3, 773.15)
+    q_m3s = q_lpm / 60_000
+    us = q_m3s / ac
+
+    # TODO: calculate gas properties
+    umf = cm.umf_ergun(d_p, emf, 3.6e-5, phi_p, rho_g, rho_p)
+
+    d_bub = 0.00853 * (1 + 27.2 * (us - umf))**(1 / 3) * (1 + 6.84 * z)**(1.21)
+    u_bub = 12.51 * ((us - umf)**0.362) * ((d_bub / d)**0.52) * d
+    v_bub = 1.285 * ((d_bub / d)**1.52) * d
+
+    rho_ss = rho_cc + rho_bb
+    ysc = rho_cc / rho_ss
+    rho_s = ((ysc / rho_c) * ((1 - ysc) / rho_b))**-1
+
+    f_bub = -(1 - emf) * rho_p * theta_w * v_bub[0:ni] * (-u_bub[1:ni + 1] + u_bub[0:ni]) / dz[0:ni]
+
+    # Momentum exchange with opposite flow βg,𝗌
+    # ------------------------------------------------------------------------
+    # psi       biomass shrinkage factor, Ѱ [-]
+    # d_s       average diameter of solid fuel particle, d𝗌 [m]
+    # dr
+    # beta_gs   momentum transfer coefficient due to gas drag, βg,𝗌 [N⋅s/m⁴]
+
+    # TODO: calculate gas velocity and gas viscosity
+    u = us
+    mu_g = 3.6e-5
+
+    psi = rho_c / (rho_b * (wc + wa))
+    d_s = d_b / (1 + (1.25 * (nf1 * psi * (1 - xc))**(1 / 3) - 1) * ysc)
+
+    re_s = (rho_g * d_s / mu_g) * np.abs(u + v)
+    cd1 = (24 / re_s) * (1 + 8.1716 * np.exp(-4.0655 * psi_s)) * (re_s**(0.0964 + 0.5565 * psi_s))
+    cd2 = (73.69 * re_s * np.exp(-5.0748 * psi_s)) / (re_s + 5.378 * np.exp(6.2122 * psi_s))
+    cd = cd1 + cd2
+
+    dr = (1 / 8) * np.pi * (d_s**2) * rho_g * cd * np.abs(u + v)
+
+    beta_gs = (6 * dr) / (np.pi * d_s**3)
+
+    # Momentum exchange with inert particles β𝗉,𝗌
+    # ------------------------------------------------------------------------
+    # d_bub_avg bubble diameter averaged over the bed height, ḏʙ [m]
+    # de        bed expansion, Δe [-]
+    # ef        bed voidage at fluidized state, e𝚏 [-]
+    # alpha_s   solids volume fraction of fuel particles, ⍺𝗌 [-]
+    # alpha_p   solids volume fraction of inert bed material, ⍺𝗉 [-]
+    # go        radial distribution function [-]
+    # beta_ps   momentum transfer coefficient due to collision with inert bed particles, β𝗉,𝗌 [N⋅s/m⁴]
+
+    d_bub_avg = np.mean(d_bub[0:ni])
+    de = (1 - 0.103 * ((us - umf)**-0.362) * (d_bub_avg / d))**(-1) - 1
+    ef = 1 - (1 - emf) / (1 + de)
+
+    alpha_s = rho_ss / rho_s
+    alpha_p = 1 - ef - alpha_s
+
+    go = (1 / ef) + ((3 * d_s * d_p) / (ef**2 * (d_s + d_p))) * ((alpha_s / d_s) + (alpha_p / d_p))
+
+    beta_ps1 = 3 * np.pi * (1 + e) * (0.5 + (mu_c * np.pi / 8)) * (d_s + d_p)**2
+    beta_ps2 = (rho_p * d_p**3) + (rho_s * d_s**3)
+    beta_ps = (beta_ps1 / beta_ps2) * alpha_p * rho_p * rho_ss * go * np.abs(v)
+
+    # Solid fuel velocity, v [m/s]
     # ------------------------------------------------------------------------
 
-    # char mass fraction and density of solid fuel particle
-    yc = rhocc / (rhocc + rhobb)
-    rhos = ((yc / rhoc) + (1 - yc) / rhob)**(-1)
-
-    # below bed top
     dvdt = np.zeros(n)
+
     dvdt[0:ni] = (
         - v[0:ni] * (v[0:ni] - v[1:ni + 1]) / dz[0:ni]
-        + g * (rhos[0:ni] - rhog) / rhos[0:ni]
-        - (1 - emf) * rhop * fw * vb[0:ni] * (-ub[1:ni + 1] + ub[0:ni]) / (rhos[0:ni] * dz[0:ni])
-        + v[0:ni] * (sb[0:ni] + sc[0:ni]) / rhos[0:ni]
+        + g * (rho_s[0:ni] - rho_g) / rho_s[0:ni]
+        + f_bub / rho_s[0:ni]
+        # + beta_gs[0:ni] * (-u - v[0:ni]) / rho_s[0:ni]
+        + beta_ps[0:ni] * (-v[0:ni]) / rho_s[0:ni]
+        + v[0:ni] * (sb[0:ni] + sc[0:ni]) / rho_s[0:ni]
     )
 
     dydt = np.concatenate((drhobbdt, drhoccdt, dvdt))
--- bfb/solver.py
+++ bfb/solver.py
@@ -1,4 +1,3 @@
-import chemics as cm
 import logging
 import numpy as np
 from scipy.integrate import solve_ivp
@@ -10,20 +9,30 @@ def solver(params):
     Solve one-dimensional biomass pyrolysis model.
     """
 
-    # One-dimensional grid
+    # Parameters
     # ------------------------------------------------------------------------
+
+    tf = params['tf']       # final time [s]
 
     ls = params['ls']       # distance to solid fuel inlet [m]
     lst = params['lst']     # static bed height [m]
     lt = params['lt']       # reactor height [m]
-    lp = lst - ls           # distance from solid fuel inlet to bed top [m]
 
     n1 = params['n1']       # number of grid points below fuel inlet
     n2 = params['n2']       # number of grid points above fuel inlet
     n3 = params['n3']       # number of grid points in freeboard
+
+    d_si = params['d_si']       # inner diameter of solid fuel inlet tube [m]
+    mf = params['mf']           # biomass inlet feed flow rate [kg/s]
+    rho_b = params['rho_b']     # biomass density [kg/m³]
+
+    # One-dimensional grid
+    # ------------------------------------------------------------------------
+
     ni = n1 + n2            # grid point at top of bed
     n = n1 + n2 + n3        # grid point at top of reactor
 
+    lp = lst - ls           # distance from solid fuel inlet to bed top [m]
     dz1 = ls / n1           # dz in bed below fuel inlet [m]
     dz2 = lp / n2           # dz in bed above fuel inlet [m]
     dz3 = (lt - lst) / n3   # dz for freeboard [m]
@@ -38,49 +47,17 @@ def solver(params):
     z3 = np.linspace(lp, lt, n3)        # z for freeboard [m]
     z = np.concatenate((z1, z2, z3))    # z vector for reactor [m]
 
-    # Bubble phase
-    # ------------------------------------------------------------------------
-
-    di = params['di']       # inner diameter of the reactor [m]
-    dpp = params['dpp']     # mean diameter of bed particle [m]
-    emf = params['emf']     # void fraction of the bed [-]
-    phip = params['phip']   # sphericity of bed particle [-]
-    rhop = params['rhop']   # bed particle density [kg/m³]
-    ug = params['ug']       # velocity of inlet gas [SLM]
-
-    ac = (np.pi / 4) * (di**2)  # inner cross-section area of the reactor [m²]
-
-    # TODO: calculate rhog, this value is for nitrogen gas
-    rhog = 0.422
-
-    # TODO: define pressure and temp in parameters file
-    # superficial gas velocity at gas inlet [m/s]
-    q_lpm = cm.slm_to_lpm(ug, 101.3, 773.15)
-    q_m3s = q_lpm / 60_000
-    us = q_m3s / ac
-
-    # TODO: calculate gas properties
-    # minimum fluidization velocity [m/s]
-    umf = cm.umf_ergun(dpp, emf, 3.6e-5, phip, rhog, rhop)
-
-    db = 0.00853 * (1 + 27.2 * (us - umf))**(1 / 3) * (1 + 6.84 * z)**(1.21)
-    vb = 1.285 * ((db / di)**1.52) * di
-    ub = 12.51 * ((us - umf)**0.362) * ((db / di)**0.52) * di
-
     # Initial conditions
     # ------------------------------------------------------------------------
 
-    dis = params['dis']     # inner diameter of biomass inlet tube [m]
-    mf = params['mf']       # biomass inlet feed flow rate [kg/s]
-    rhob = params['rhob']   # biomass density [kg/m³]
-
-    ai = (np.pi / 4) * (dis**2)     # inner cross-section area of inlet [m²]
-    vi = mf / (rhob * ai)           # biomass velocity at fuel inlet [m/s]
+    ai = (np.pi / 4) * (d_si**2)    # inner cross-section area of solid fuel inlet tube [m²]
+    vi = mf / (rho_b * ai)           # biomass velocity at fuel inlet [m/s]
 
     rhobb0 = np.zeros(n)    # initial biomass mass concentration [kg/m³]
     rhobb0[:] = 1e-12
 
     rhocc0 = np.zeros(n)    # initial char mass concentration [kg/m³]
+    rhocc0[:] = 1e-12
 
     v0 = np.zeros(n)        # initial solid fuel velocity [m/s]
     v0[n1] = vi
@@ -88,12 +65,10 @@ def solver(params):
     # Solve ODEs
     # ------------------------------------------------------------------------
 
-    tf = params['tf']   # final time [s]
-    tspan = (0, tf)     # time span [s]
-
+    tspan = (0, tf)                             # time span [s]
     y0 = np.concatenate((rhobb0, rhocc0, v0))   # initial conditions vector
 
-    sol = solve_ivp(dydt, tspan, y0, method='Radau', args=(ac, dz, n, ni, ub, vb, params))
+    sol = solve_ivp(dydt, tspan, y0, method='Radau', args=(dz, z, n, ni, params))
 
     # Log
     # ------------------------------------------------------------------------
